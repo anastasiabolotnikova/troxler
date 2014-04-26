41ece4aa8e621b1c84d10fd6b4b256411ab323ba
Anton <prokopov@hotmail.com>
2014-04-27 00:24:21 +0300

    Parameters and save data works (not good, but hey!)

---
+++ save_text.php
@@ -0,0 +1,9 @@
+<?php
+$file = 'test_data.txt';
+// Open the file to get existing content
+$current = file_get_contents($file);
+// Append a new person to the file
+$current .= "John Smith\n";
+// Write the contents back to the file
+file_put_contents($file, $current);
+?>
