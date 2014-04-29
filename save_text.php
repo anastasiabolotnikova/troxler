bbe7042116871d324b159da959eda8562def8cbe
Anton <prokopov@hotmail.com>
2014-04-29 22:26:43 +0300

    Save data works
    Perhaps, we use something instead of tabulators in text file?

--- save_text.php
+++ save_text.php
@@ -1,9 +1,37 @@
 <?php
+
+$size = $_POST['size'];
+$dist = $_POST['dist'];
+$blur = $_POST['blur'];
+$color = $_POST['color'];
+$time = $_POST['time'];
+
 $file = 'test_data.txt';
 // Open the file to get existing content
 $current = file_get_contents($file);
 // Append a new person to the file
-$current .= "John Smith\n";
+
+if(isset($size) and isset($dist) and isset($blur) and isset($color) and isset($time)){
+	if($size==null){
+		$size = 10;
+	}
+	if($dist==null){
+		$dist = 160;
+	}
+	if($blur==null){
+		$blur = 5;
+	}
+	if($color==null){
+		$color = 'blue';
+	}
+	if($time==null){
+		$time = '0000';
+	}
+	$current .= $size."\t".$dist."\t".$blur."\t".$color."\t".$time.PHP_EOL;
+}
+else{
+	$current .= "10\t160\t5\tblue\t0000".PHP_EOL;;
+}
 // Write the contents back to the file
 file_put_contents($file, $current);
 ?>
--- script.js
+++ script.js
@@ -5,15 +5,37 @@ function timeStart(){
 	document.getElementById('butt').onclick = function(){(timeStop())};
 	document.getElementById('butt').value = "Stop";
 }
-
+var sel_time = null;
 function timeStop(){
 	var stop = new Date().getTime();
 	var diff = stop - start;
 	var s = parseInt(diff, 10);
+	sel_time = s;
 	
-	//document.write(start);
-	//document.write(stop);
 	document.getElementById('timeHere').innerHTML = s;
 	document.getElementById('butt').onclick = function(){(timeStart())};
 	document.getElementById('butt').value = "Start";
+}
+var sel_size = null;
+var sel_dist = null;
+var sel_blur = null;
+var sel_color = null;
+
+function prepareVars() {
+    sel_size = $('#sel_size').val();
+	sel_dist = $('#sel_dist').val();
+	sel_blur = $('#sel_blur').val();
+	sel_color = $('#sel_color').val();
+}
+
+function saveData(){
+	$.ajax({
+        url:        'save_text.php',
+        type:       'POST',
+        dataType:   'json',
+        data:       { size: sel_size, dist: sel_dist, blur: sel_blur, color: sel_color, time: sel_time },
+        success:    function(data) {
+            alert(data);
+        }
+    });
 }
