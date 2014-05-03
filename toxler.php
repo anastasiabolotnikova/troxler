f2dfa95cba4f16b925ae02aef18a3bedc3dc1c85
anastasiabolotnikova <bolotnikova.anastasia@gmail.com>
2014-05-04 00:55:10 +0300

    Implemented predefined sets of parameters
    Save data for predefined sets of parameters does not work

--- toxler.php
+++ toxler.php
@@ -12,14 +12,45 @@
         <h1>Welcome to the Toxler Effect Test Environment</h1>
 		<div class="toxler"> <div class="fixPoint"></div>';
 			//<?php
-			if(isset($_POST['dist']) and isset($_POST['size']) and isset($_POST['blur']) and isset($_POST['color'])){
+			if (isset($_POST['blur']) and isset($_POST['size']) and isset($_POST['blur']) and isset($_POST['color'])){
+				// not predefined param-s
 				$distance = $_POST['dist'];
 				$size = $_POST['size'];
 				$blur = $_POST['blur'];
 				$color = $_POST['color'];
-			} else {
+			}
+			else if(isset($_POST['blur'])){ 
+				// testing blurriness impact
 				$distance = 160;
-				$size = 10;
+				$size = 30;
+				$blur = $_POST['blur'];
+				$color = "blue";
+			} 
+			// testing color impact
+			else if (isset($_POST['color'])){
+				$distance = 160;
+				$size = 30;
+				$blur = 15;
+				$color = $_POST['color'];
+			} 
+			// testing size impact
+			else if (isset($_POST['size'])){
+				$distance = 160;
+				$size = $_POST['size'];
+				$blur = 15;
+				$color = "blue";
+			} 
+			// testing distance impact
+			else if (isset($_POST['dist'])){
+				$distance = $_POST['dist'];
+				$size = 30;
+				$blur = 15;
+				$color = "blue";
+			} 
+			// Default param-s
+			else {
+				$distance = 160;
+				$size = 30;
 				$blur = 5;
 				$color = 'blue';
 			}
